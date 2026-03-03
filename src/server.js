17fd575c04cefe724bf95d542b9f7dc9b5e00665
tan-koo <tanabat.documents@mail.kmutt.ac.th>
2026-03-03 15:40:19 +0700

    Update port

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
   res.send("EA License API running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
