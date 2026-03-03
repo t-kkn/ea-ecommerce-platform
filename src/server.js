ac1fd008e9e4e874a634ccb262fe146c88adba17
tan-koo <tanabat.documents@mail.kmutt.ac.th>
2026-03-03 15:16:30 +0700

    edit server

--- src/server.js
+++ src/server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/health", (_req, res) => {
+  res.status(200).json({ ok: true });
+});
+
 app.get("/", (req, res) => {
   res.send("EA License API running...");
 });
